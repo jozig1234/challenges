b02fdce245793ab2ea6496e02ba76f31bc858d81
Emanuel Raad <raademang@gmail.com>
2013-05-18 11:29:17 -0400

    First challenge
    Tests knowledge of basic python concepts.

---
+++ interview.py
@@ -0,0 +1,19 @@
+'''
+Created on 2013-05-18
+
+@author: Emanuel
+
+Create a program that asks the user for his
+name and his age. With that information,
+calculate his year of birth.
+
+Output the information to the console
+in this format:
+You name is (blank) and you are (blank) years old.
+You were born in (blank).
+
+If he is one year old, write 'and you are 1 year old'.
+If he us zero years old, write 'and you are still a baby!'
+If he enters a negative number, say something mean. Don't tell
+him what year he was born in either.
+'''
