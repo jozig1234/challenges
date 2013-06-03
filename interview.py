0cc6a491d704e1d6016be8b528dcc76c1f2a8172
jozig1234 <jjourekian@hotmail.com>
2013-06-03 19:30:47 -0300

    both challenges completed:)

--- interview.py
+++ interview.py
@@ -24,3 +24,60 @@ Your name is manny and you are 16 years old.
 You were born in 1997.
 --------------------
 '''
+heres both int and str challenges completed.
+int
+def exposant(n):
+      return n**2
+
+#Étant donné un nombre, retourne ce nombre modulo deux. Que remarque tu?
+def modulo(n):
+    return n%2
+
+#Ignorez le restant
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+    
+def main():
+    print 'Exposant:'
+    test(exposant(2), 4)
+    test(exposant(8), 64)
+    test(exposant(0), 0)
+    test(exposant(15), 225)
+
+    print 'Modulo:'
+    test(modulo(5), 1)
+    test(modulo(10), 0)
+    test(modulo(27), 1)
+    test(modulo(8), 0)
+
+
+if __name__ == '__main__':
+    main()
+    
+str
+
+def bonjour(nom):
+    n = "Bonjour " + nom + "!"
+    return n
+
+
+#Ignorez le restant
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+    
+def main():
+    test(bonjour('Scout'), 'Bonjour Scout!')
+    test(bonjour('Jem'), 'Bonjour Jem!')
+    test(bonjour('Dill'), 'Bonjour Dill!')
+    test(bonjour('Atticus'), 'Bonjour Atticus!')
+
+if __name__ == '__main__':
+    main()
